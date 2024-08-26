e92c67cd429aa1ad7294e7b29bbd2371aaf7ce0d
zhqihang <qihang9903@163.com>
2024-08-26 14:35:15 +0800

    初始化

---
+++ src/main/java/qihang/wxlogin/WxloginApplication.java
@@ -0,0 +1,13 @@
+package qihang.wxlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxloginApplication.class, args);
+    }
+
+}
